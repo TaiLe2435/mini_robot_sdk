5cc13815bb58873075c02ba4d6b669886fe5b05b
Tommy Le <TaiLe2435@gmail.com>
2023-12-06 15:27:22 -0600

    added comments

--- include/motion/motion.cpp
+++ include/motion/motion.cpp
@@ -4,13 +4,18 @@
 #include <DRV8835MotorShield.h>
 
 const int LED_PIN {2};
-const int M1DIR {27};
-const int M1PWM {15};
-const int M2DIR {25};
-const int M2PWM {26};
-const int MD {23};
+const int M1DIR {27}; // APHASE
+const int M1PWM {15}; // AENBL
+const int M2DIR {25}; // BPHASE
+const int M2PWM {26}; // BENBL
+const int MD {23};    // MODE
 
 DRV8835MotorShield motors(M1DIR, M1PWM, M2DIR, M2PWM);
+
+/*//////////////////////////////////////////////////////////////////
+// init_motors() : initializes motor directions
+// uncomment line corresponding to motor to flip direction
+/////////////////////////////////////////////////////////////////*/
 
 void init_motors()
 {
@@ -20,12 +25,22 @@ void init_motors()
     return;
 }
 
+/*//////////////////////////////////////////////////////////////////
+// stop() : motor braking
+// Brakes by setting speed to 0
+/////////////////////////////////////////////////////////////////*/
+
 void stop()
 {
     motors.setM1Speed(0);
     motors.setM1Speed(0);
     return;
 }
+
+/*//////////////////////////////////////////////////////////////////
+// move(int v, int theta) : commands motor speeds for given v and theta
+// NOT DONE
+/////////////////////////////////////////////////////////////////*/
 
 void move(int v, int theta)
 {
@@ -52,6 +67,11 @@ void move(int v, int theta)
     } 
     return;
 }
+
+/*//////////////////////////////////////////////////////////////////
+// move_wheels(int vL, int vR) : low-level motor control
+// individual  wheel speed motor command
+/////////////////////////////////////////////////////////////////*/
 
 void move_wheels(int vL, int vR)
 {
--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,48 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-// Header files
-#include "IMU/IMU.h"
-#include "motion/motion.h"
-#include "bluetooth/bluetooth.h"
+// // Header files
+// #include "IMU/IMU.h"
+// #include "motion/motion.h"
+// #include "bluetooth/bluetooth.h"
 
-float gyroCalib[3] {0};
+// float gyroCalib[3] {0};
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("setting up :)");
+// void setup() {
+//   Serial.begin(115200);
+//   Serial.println("setting up :)");
 
-  float* calib = initIMU();
-  gyroCalib[0] = calib[0];
-  gyroCalib[1] = calib[1];
-  gyroCalib[2] = calib[2];
+//   float* calib = initIMU();
+//   gyroCalib[0] = calib[0];
+//   gyroCalib[1] = calib[1];
+//   gyroCalib[2] = calib[2];
 
-  BTinit();
-}
+//   BTinit();
+// }
 
-void loop() {
-  Serial.println("looping ... <(o.o)>");
+// void loop() {
+//   Serial.println("looping ... <(o.o)>");
 
-  int desired = getBT();
+//   int desired = getBT();
 
-  float heading = poseEstimation(gyroCalib[0], gyroCalib[1], gyroCalib[2]);
+//   float heading = poseEstimation(gyroCalib[0], gyroCalib[1], gyroCalib[2]);
 
-  int kP = 0.5;
-  if(desired != 9999)
-  {
-    float error = desired - (int)heading;
-    float leftSpeed = 200 + kP*error;
-    float rightSpeed = 200 - kP*error;
-    move_wheels(leftSpeed, rightSpeed);
-  }
-  else if (desired == 9999)
-  {
-    stop();
-  }
-  else
-  {
-    stop();
-    Serial.println("Error in data received: Unknown value received");
-    while(1);
-  }
-}
+//   float kP {0.5};
+//   if(desired != 9999)
+//   {
+//     float error = desired - (int)heading;
+//     float leftSpeed = 200 + kP*error;
+//     float rightSpeed = 200 - kP*error;
+//     move_wheels(leftSpeed, rightSpeed);
+//   }
+//   else if (desired == 9999)
+//   {
+//     stop();
+//   }
+//   else
+//   {
+//     stop();
+//     Serial.println("Error in bluetooth data: unknown value received");
+//     while(1);
+//   }
+// }
 
