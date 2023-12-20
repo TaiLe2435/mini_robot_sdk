d3b5033f38324c3fe0f938e76ddeac5d10dd9eb1
Tommy Le <TaiLe2435@gmail.com>
2023-12-19 19:33:08 -0600

    reorganized directory

--- mini_robot_sdk/include/bluetooth/bluetooth.h
+++
@@ -1,5 +0,0 @@
-#include <Arduino.h>
-
-void BTinit();
-
-int getBT();
--- mini_robot_sdk/include/motion/motion.h
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-
-void init_motors();
-
-void stop();
-
-void move(int v, int theta);
-
-void move_wheels(int vL, int vR);
